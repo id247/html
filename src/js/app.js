9a1c55c835779cb213badf939d2ca13cf2be6f0a
simplecss <simplecss@yandex.ru>
2016-09-01 19:33:21 +0300

    awesome config

--- src/js/app.js
+++ src/js/app.js
@@ -105,25 +105,6 @@ export default (function (window, document, $){
 
 	function form(){		
 
-		$.extend($.validator.messages, {
-			required: 'Это поле обязательно для заполнения.',
-			remote: 'Please fix this field.',
-			email: 'Введите корректный e-mail адрес.',
-			url: 'Please enter a valid URL.',
-			date: 'Please enter a valid date.',
-			dateISO: 'Please enter a valid date (ISO).',
-			number: 'Введите число.',
-			digits: 'Допустимо вводить только цифры.',
-			creditcard: 'Please enter a valid credit card number.',
-			equalTo: 'Please enter the same value again.',
-			accept: 'Please enter a value with a valid extension.',
-			maxlength: jQuery.validator.format('Please enter no more than {0} characters.'),
-			minlength: jQuery.validator.format('Please enter at least {0} characters.'),
-			rangelength: jQuery.validator.format('Please enter a value between {0} and {1} characters long.'),
-			range: jQuery.validator.format('Please enter a value between {0} and {1}.'),
-			max: jQuery.validator.format('Please enter a value less than or equal to {0}.'),
-			min: jQuery.validator.format('Please enter a value greater than or equal to {0}.')
-		});
 
 		$('form').each( function(){
 
@@ -133,18 +114,12 @@ export default (function (window, document, $){
 			
 			$success.hide();
 
-			$form.validate({
-			});
-
 			$form.on('submit', function(e){
 
 				e.preventDefault();
 
 				const form = e.target;
 
-				if ( !$(form).valid() ){
-					return false;
-				}
 
 				$button.text('Отправка данных...');
 				$button.attr('disabled', true);
